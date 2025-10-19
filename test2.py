6bcd399d5b5e2db7c0f2f99a265a384b1e97cba3
Nick Piet <nick@npiet.com>
2025-10-19 17:34:13 +1100

    modified:   test2.py

--- test2.py
+++ test2.py
@@ -15,17 +15,23 @@ SAVING_RATE = (c1 + c2) * n
 MONTHLY_INTEREST = int_rate / (12 * 100)
 COMPOUNDING_INTEREST = (c1 + c2) * ((1 + MONTHLY_INTEREST)**n - 1) / MONTHLY_INTEREST
 RS_BAL = log(1 + (COMPOUNDING_INTEREST * MONTHLY_INTEREST / (c1 + c2))) / log(1 + MONTHLY_INTEREST)
+sleep(1)
+system('cls')
 
+print("\n")
 print(f"""
+        RESULTS:
+        --------
         Person 1 savings (per month):\t${c1}
         Person 2 savings (per month):\t${c2}\n
-        INTEREST
+        INTEREST:
         -------
         Yearly interest rate:\t{int_rate}%
-        Which works out to\t{MONTHLY_INTEREST:,.4f}% per month.\n
+        Which works out to\t{MONTHLY_INTEREST * 100:,.2f}% per month.\n
 
         In {n:,.0f} months, you will save:
         Without interest:\t${SAVING_RATE:,.2f}
-        With interest:\t${COMPOUNDING_INTEREST:,.2f}
+        With interest:\t\t${COMPOUNDING_INTEREST:,.2f}
+        Earned interest:\t${COMPOUNDING_INTEREST - SAVING_RATE:,.2f} in {n:,.0f} months.
 
       """)
